5073543122a9b8d043db23a5c7c2dbad9ce945db
CrazyMax <crazy-max@users.noreply.github.com>
2022-03-15 15:27:09 +0100

    control: add info service
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

---
+++ client/info.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"context"
+
+	controlapi "github.com/moby/buildkit/api/services/control"
+	"github.com/pkg/errors"
+)
+
+type InfoResponse struct {
+	BuildkitPackage  string
+	BuildkitVersion  string
+	BuildkitRevision string
+}
+
+func (c *Client) Info(ctx context.Context) (*InfoResponse, error) {
+	res, err := c.controlClient().Info(ctx, &controlapi.InfoRequest{})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to call info")
+	}
+	return &InfoResponse{
+		BuildkitPackage:  res.BuildkitPackage,
+		BuildkitVersion:  res.BuildkitVersion,
+		BuildkitRevision: res.BuildkitRevision,
+	}, nil
+}
